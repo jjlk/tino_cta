6c9de934d01e78c55b4370678d803bce83018d30
jlk <julien.lefaucheur@obspm.fr>
2018-04-12 15:04:32 +0200

    Modify merge table

--- snippets/append_tables.py
+++ snippets/append_tables.py
@@ -18,7 +18,7 @@ except ImportError:
     print("no pandas installed?")
 
 
-def merge_list_of_pytables(filename_list, destination):
+def merge_list_of_pytables(filename_list, destination, table_name):
     pyt_table = None
     outfile = tb.open_file(destination, mode="w")
     for i, filename in enumerate(sorted(filename_list)):
@@ -28,26 +28,28 @@ def merge_list_of_pytables(filename_list, destination):
 
         if i == 0:
             pyt_table = pyt_infile.copy_node(
-                where='/', name='reco_events', newparent=outfile.root)
+                where='/', name=table_name, newparent=outfile.root)
 
         else:
-            pyt_table_t = pyt_infile.root.reco_events
+            #pyt_table_t = pyt_infile.root.reco_events
+            # JLK hacked...
+            pyt_table_t = pyt_infile.root.feature_events_LSTCam
             pyt_table_t.append_where(dstTable=pyt_table)
 
     print("merged {} files".format(len(filename_list)))
     return pyt_table
 
 
-def merge_list_of_pandas(filename_list, destination):
+def merge_list_of_pandas(filename_list, destination, table_name):
     store = pd.HDFStore(destination)
     for i, filename in enumerate(sorted(filename_list)):
         s = pd.HDFStore(filename)
-        df = pd.read_hdf(filename, 'reco_events')
+        df = pd.read_hdf(filename, table_name)
         if i == 0:
-            store.put('reco_events', df, format='table', data_columns=True)
+            store.put(table_name, df, format='table', data_columns=True)
         else:
-            store.append(key='reco_events', value=df, format='table')
-    return store['reco_events']
+            store.append(key=table_name, value=df, format='table')
+    return store[table_name]
 
 
 if __name__ == "__main__":
@@ -57,7 +59,13 @@ if __name__ == "__main__":
     parser.add_argument('--infiles_base', type=str, default="classified_events")
     parser.add_argument('--auto', action='store_true', dest='auto', default=False)
     parser.add_argument('-o', '--outfile', type=str)
+    parser.add_argument('--table_name', type=str, default='reco_events')
     args = parser.parse_args()
+
+    print('DEBUG> infiles_base={}'.format(args.infiles_base))
+    print('DEBUG> indir={}'.format(args.indir))
+    print('DEBUG> outfile={}'.format(args.outfile))
+    print('DEBUG> auto={}'.format(args.auto))
 
     if args.auto:
         for channel in ["gamma", "proton"]:
@@ -75,4 +83,4 @@ if __name__ == "__main__":
         filename_list = glob.glob(input_template)
         print("filename_list:", filename_list)
 
-        merge_list_of_pytables(filename_list, args.outfile)
+        merge_list_of_pytables(filename_list, args.outfile, args.table_name)
